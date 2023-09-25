2a563555bf18c4c86b40c791799fe1b191a1fec2
tajirri <clarencewagah@gmail.com>
2023-09-25 08:51:17 -0700

    more malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * malloc_checked -  process termination with a status value of 98
+ * malloc_checked - cause normal process termination with a status value of 98
  *
  * @b: allocated memory
  *
