08d48266b04176562a8886c26534143f413f1151
tajirri <clarencewagah@gmail.com>
2023-09-27 12:14:48 -0700

    poppy second commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,17 @@
 #ifndef DOG
 #define DOG
 
-/**
- * struct dog - dog class
- * name: Name of dog
- * age: Age of the dog
- * owner: Owner of the dog
- */
+#ifndef _HEADER_
+#define _HEADER_
 
+/**
+ * struct dog - Dog attributes
+ * name: The name of the dog.
+ * age: The age of the dog.
+ * owner: The owner of the dog.
+ *
+ * Description: The attributes of a dog.
+ */
 struct dog
 {
 	char *name;
@@ -15,13 +19,14 @@ struct dog
 	char *owner;
 };
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
 
 typedef struct dog dog_t;
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-
 void free_dog(dog_t *d);
+
+#endif
 
 #endif
