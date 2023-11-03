80c99f86a5ac9417267db46df6f2d553177ef1d1
tajirri <clarencewagah@gmail.com>
2023-11-02 19:59:28 -0700

    Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * malloc_checked
+ *
+ * @b: allocated memory
+ *
+ * Return: pointer to allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *i;
+
+	i = malloc(b);
+	if (i == NULL)
+		exit(98);
+	return (i);
+
+}
