577a11d47ec5e03b7f7bd5423019d3aa8222cca9
tajirri <clarencewagah@gmail.com>
2023-11-16 18:54:56 -0800

    Print List

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,7 +5,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-size_t print_list(const list_t *h);
 
 /**
  * struct list_s - singly linked list
@@ -22,5 +21,8 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+
+size_t print_list(const list_t *h);
+
 
 #endif
