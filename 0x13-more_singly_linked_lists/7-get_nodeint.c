ac03c74ac53e79a2b83832e804f7b42b97be0936
tajirri <clarencewagah@gmail.com>
2023-11-21 18:51:03 -0800

    Get node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,8 +16,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	for (i = 0; i < index; i++)
 	{
 		head = head->next;
-		if (head == NULL)
+		if (next == NULL)
 			return (NULL);
 	}
-	return (head)
+	return (head);
 }
