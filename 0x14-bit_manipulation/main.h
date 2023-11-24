f6c12ff7fa611b1e3d8d52a848cd32cae0b5f62a
tajirri <clarencewagah@gmail.com>
2023-11-23 18:06:41 -0800

    1.1

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,7 +3,6 @@
 
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
-unsigned long int _power(unsigned int base, unsigned int pow);
-
+int _putchar(char c);
 
 #endif
